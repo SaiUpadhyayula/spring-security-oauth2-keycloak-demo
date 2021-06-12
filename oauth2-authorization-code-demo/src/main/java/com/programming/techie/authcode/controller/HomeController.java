8d9ee7c991db34c98a496831d4d0d6026052fd23
Sai Upadhyayula <subramanyam.sai90@gmail.com>
2021-06-12 23:15:38 +0200

    Changed Port to 8080 and removed unused Exception

--- oauth2-authorization-code-demo/src/main/java/com/programming/techie/authcode/controller/HomeController.java
+++ oauth2-authorization-code-demo/src/main/java/com/programming/techie/authcode/controller/HomeController.java
@@ -3,13 +3,11 @@ package com.programming.techie.authcode.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.net.URISyntaxException;
-
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String home() throws URISyntaxException {
+    public String home() {
         return "home";
     }
 }
